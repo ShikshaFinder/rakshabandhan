3366364c2a9f1ea1164d14f25c31e9647670479a
ShikshaFinder <janiharsh794@gmail.com>
2023-08-29 23:49:53 +0530

    ha ha na na na na

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,17 @@
 import Meet from "@/lib/component/Meet";
 import Googlepay from "@/lib/component/googlepay";
+import { Analytics } from "@vercel/analytics/react";
+
 
 import { useState } from "react";
 export default function sf() {
   const [a, b] = useState("harsh");
   return (
     <>
+     
+      <Analytics />
       <Meet />
-      <Googlepay/>
+      <Googlepay />
     </>
   );
 }
